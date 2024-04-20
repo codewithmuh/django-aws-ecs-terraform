4e7051508467275fe7bd169007c4f562afbcdb84
Muhammad Rashid <rashiddaha80@gmail.com>
2024-04-20 10:29:58 +0500

    updates

--- django/build-process/docker-backend-django/scripts/boot.sh
+++ django/build-process/docker-backend-django/scripts/boot.sh
@@ -3,11 +3,17 @@ set -e
 
 echo "codewithmuh-backend:boot:env:${APP_ENVIRONMENT}"
 
-python manage.py makemigrations
-
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-if [ "$APP_ENVIRONMENT" == "Local" ]; then
-  echo "codewithmuh-backend:run:local" && python manage.py runserver 0.0.0.0:8080 --insecure
+if [ "$APP_ENVIRONMENT" != "Production" ]; then
+  echo "agentloop-backend:run:local" && python manage.py runserver 0.0.0.0:8080 --insecure
+fi
+
+if [ "$APP_ENVIRONMENT" == "Production" ]; then
+  echo "agentloop-backend:run:prod" && /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisor-backend.conf
+fi
+
+if [ "$APP_ENVIRONMENT" == "Staging" ]; then
+  echo "agentloop-backend:run:stag" && /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisor-backend.conf
 fi
