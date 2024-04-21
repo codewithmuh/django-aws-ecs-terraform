a5e17361cc16eb5951e2b5f8845f8c5552f713a1
Muhammad Rashid <rashiddaha80@gmail.com>
2024-04-21 17:54:19 +0500

    ok

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -38,7 +38,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
 ]
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["http://ecs-load-balancer-178276710.ap-south-1.elb.amazonaws.com"]
 
 CORS_ALLOW_HEADERS = [
     'accept',
